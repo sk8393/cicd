4da2c0dc167f17fd4960b945fc2b48d04351cdde
Ubuntu <ubuntu@ip-172-31-10-137.ap-northeast-1.compute.internal>
2021-04-23 08:06:57 +0000

    Modified base00/sample.py to support database and Elasticsearch interaction.

--- base00/sample.py
+++ base00/sample.py
@@ -6,24 +6,36 @@ import time
 from fluent import sender
 from fluent import event
 
-unixtime = int(time.time())
-print(unixtime)
+time.sleep(30)
 
-ec2 = boto3.client('ec2')
-responce = ec2.describe_instances()
+while 1:
+    _timestamp = int(time.time())
+    print(_timestamp)
 
-for reservation in responce['Reservations']:
-    for instance in reservation['Instances']:
-        instance_id = instance['InstanceId']
-        print(instance_id)
+    ec2 = boto3.client('ec2')
+    responce = ec2.describe_instances()
 
-        body_dict = dict()
-        body_dict['id'] = instance_id
+    for reservation in responce['Reservations']:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+            print(instance_id)
+            _id = instance_id
 
-        sender.setup(
-                'awssql',
-                host = 'localhost',
-                port=8888)
-        event.Event(
-                'ec2',
-                body_dict)
+            connection = psycopg2.connect("host={0} port={1} user={2} password={3} dbname={4}".format("postgres", "5432", "root", "acd483de", "awssql"))
+            cursor = connection.cursor()
+            cursor.execute('INSERT INTO root.ec2_describe_instances_instances (_id, _timestamp) VALUES (%s, %s);', (instance_id, _timestamp))
+            connection.commit()
+
+            body_dict = dict()
+            body_dict['instance_id'] = instance_id
+            body_dict['message'] = "test message to elasticsearch"
+
+            sender.setup(
+                    'awssql',
+                    host = 'localhost',
+                    port=8888)
+            event.Event(
+                    'rds',
+                    body_dict)
+
+    time.sleep(10)
