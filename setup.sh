274bfcc3244dc0409c7df8bd5e15db66d285e2c0
Ubuntu <ubuntu@ip-172-31-10-129.ap-northeast-1.compute.internal>
2021-04-24 08:05:56 +0000

    Modified setup.sh to reflect remaining steps.

--- setup.sh
+++ setup.sh
@@ -5,3 +5,13 @@
 git config --global core.editor vim
 
 # Create .aws/credentials with AWS access key.
+
+sudo ./create_docker_image_gitbucket.sh
+sudo ./create_docker_image_jenkins.sh
+sudo ./create_docker_compose_project_cicd.sh
+
+sudo docker ps -a | grep jenkins | cut -d " " -f1 | xargs sudo docker logs
+
+# Inbound rule of security group has to be opene for all traffic.
+cd jenkins;./create_import.sh.sh
+cd jenkins;./import.sh
