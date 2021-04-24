5fa90dd739372eda5ffc94abea7a6136901194cc
Ubuntu <ubuntu@ip-172-31-10-129.ap-northeast-1.compute.internal>
2021-04-24 06:23:42 +0000

    Modified setup.sh to reflect information of core.editor.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 # Set up local branch that corresponds to existing remote branch.
 ./fetch.sh
+
+# nano is the default editor on Ubuntu that I am not familiar with.
+git config --global core.editor vim
