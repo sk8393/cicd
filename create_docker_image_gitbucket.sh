15139e5ece59d88fd49e1d6c316089f05f351cba
Ubuntu <ubuntu@ip-172-31-10-240.ap-northeast-1.compute.internal>
2021-04-25 03:40:16 +0000

    Modified docker-compose.yml to change tag from 2.0 to production.

--- create_docker_image_gitbucket.sh
+++ create_docker_image_gitbucket.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t sk8393/gitbucket:2.0 -f gitbucket/Dockerfile .
+docker build -t sk8393/gitbucket:production -f gitbucket/Dockerfile .
--- create_docker_image_jenkins.sh
+++ create_docker_image_jenkins.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t sk8393/jenkins:2.0 -f jenkins/Dockerfile .
+docker build -t sk8393/jenkins:production -f jenkins/Dockerfile .
