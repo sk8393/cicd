3593122221ee9808067bd6835f8869848a74c218
Ubuntu <ubuntu@ip-172-31-10-65.ap-northeast-1.compute.internal>
2021-04-27 07:21:18 +0000

    Created requester/util.py.

--- requester/sample.py
+++ requester/sample.py
@@ -3,13 +3,24 @@ import pprint
 import time
 import uuid
 
+from util import DB
+from util import show_message
+
 _timestamp = int(time.time())
-print(_timestamp)
 
 ec2 = boto3.client('ec2')
 responce = ec2.describe_instances()
 
 pseudo_table_dict = {}
+
+db = DB()
+VARCHAR_LENGTH = '64'
+
+def avoid_key_name(_arg_column_name):
+    if _arg_column_name.lower() == "primary":
+        return "_primary"
+    else:
+        return _arg_column_name
 
 def get_dict_hierarchy(target_dict, root_path, sep):
     if isinstance(target_dict, dict):
@@ -28,94 +39,108 @@ def dig_dict(target_dict, target_branch, sep):
         leaf = leaf[one_limb]
     return leaf
 
-def _f(_arg_instance_dict, _arg_table, _arg_index, _arg_id, _arg_timestamp, _arg_join_key):
-    for _k in list(get_dict_hierarchy(_arg_instance_dict, '', '_')):
-        if isinstance(dig_dict(_arg_instance_dict, _k, '_'), list):
+def _flatten(_arg_instance_dict, _arg_table_name, _arg_id, _arg_timestamp, _arg_index, _arg_join_key):
+    for _column_name in list(get_dict_hierarchy(_arg_instance_dict, '', '_')):
+        if isinstance(dig_dict(_arg_instance_dict, _column_name, '_'), list):
             _join_key = str(uuid.uuid4())
-            for _i_sub, _d_sub in enumerate(dig_dict(_arg_instance_dict, _k, '_')):
-                # print("_table %s" % (_arg_table.lower()))
-                # print("_id: %s" % (_arg_id))
-                # print("_timestamp: %s" % (_arg_timestamp))
-                # print("key: %s" % (_k.lower()))
-                # print("value: %s" % (_join_key))
-                # if _arg_index is not None:
-                #     print("_index: %s" % (_arg_index))
-                # if _arg_join_key is not None:
-                #     print("_join_key: %s" %(_arg_join_key))
-                tmp_pseudo_table_dict = {(_arg_table.lower(), _arg_index): {_k.lower(): _join_key}}
-                print("tmp_pseudo_table_dict: %s" % (tmp_pseudo_table_dict))
-                # pseudo_table_dict[(_arg_table.lower(), _arg_index)] = {_k.lower(): _join_key}
-                # if pseudo_table_dict[(_arg_table.lower(), _arg_index)]:
-                #     tmp = pseudo_table_dict[(_arg_table.lower(), _arg_index)]
-                #     tmp[_k.lower()] = _join_key
-                #     pseudo_table_dict[(_arg_table.lower(), _arg_index)] = tmp
-                # else:
-                #     pseudo_table_dict[(_arg_table.lower(), _arg_index)] = {}
+            for _index_sub, _dict_sub in enumerate(dig_dict(_arg_instance_dict, _column_name, '_')):
+                tmp_pseudo_table_dict = {(_arg_table_name.lower(), _arg_index): {_column_name.lower(): _join_key}}
 
                 try:
-                    pseudo_table_dict[(_arg_table.lower(), _arg_index)]
+                    pseudo_table_dict[(_arg_table_name.lower(), _arg_index)]
                 except KeyError:
-                    pseudo_table_dict[(_arg_table.lower(), _arg_index)] = {}
+                    pseudo_table_dict[(_arg_table_name.lower(), _arg_index)] = {}
 
-                tmp = pseudo_table_dict[(_arg_table.lower(), _arg_index)]
-                tmp[_k.lower()] = _join_key
-                pseudo_table_dict[(_arg_table.lower(), _arg_index)] = tmp
-                # print()
-                _f(_d_sub, _arg_table + "_" + _k, _i_sub, _arg_id, _arg_timestamp, _join_key)
+                tmp = pseudo_table_dict[(_arg_table_name.lower(), _arg_index)]
+                tmp[_column_name.lower()] = _join_key
+                tmp["_id"] = _arg_id
+                tmp["_timestamp"] = _arg_timestamp
+                tmp["_index"] = _arg_index
+                tmp["_join_key"] = _arg_join_key
+                pseudo_table_dict[(_arg_table_name.lower(), _arg_index)] = tmp
+
+                _flatten(_dict_sub, _arg_table_name + "_" + _column_name, _arg_id, _arg_timestamp, _index_sub, _join_key)
         else:
-            flat_dict[_k] = dig_dict(_arg_instance_dict, _k, '_')
-            # print("table: %s" % (_arg_table.lower()))
-            # print("_id: %s" % (_arg_id))
-            # print("_timestamp: %s" % (_arg_timestamp))
-            # print("key: %s" % (_k.lower()))
-            # print("value: %s" % (dig_dict(_arg_instance_dict, _k, '_')))
-            # if _arg_index is not None:
-            #     print("_index: %s" % (_arg_index))
-            # if _arg_join_key is not None:
-            #     print("_join_key: %s" %(_arg_join_key))
-            tmp_pseudo_table_dict = {(_arg_table.lower(), _arg_index): {_k.lower(): (dig_dict(_arg_instance_dict, _k, '_'))}}
-            print("tmp_pseudo_table_dict: %s" % (tmp_pseudo_table_dict))
-            # pseudo_table_dict[(_arg_table.lower(), _arg_index)] = {_k.lower(): (dig_dict(_arg_instance_dict, _k, '_'))}
-            # if pseudo_table_dict[(_arg_table.lower(), _arg_index)]:
-            #     tmp = pseudo_table_dict[(_arg_table.lower(), _arg_index)]
-            #     tmp[_k.lower()] = (dig_dict(_arg_instance_dict, _k, '_'))
-            #     pseudo_table_dict[(_arg_table.lower(), _arg_index)] = tmp
-            # else:
-            #     pseudo_table_dict[(_arg_table.lower(), _arg_index)] = {}
+            tmp_pseudo_table_dict = {(_arg_table_name.lower(), _arg_index): {_column_name.lower(): (dig_dict(_arg_instance_dict, _column_name, '_'))}}
 
             try:
-                pseudo_table_dict[(_arg_table.lower(), _arg_index)]
+                pseudo_table_dict[(_arg_table_name.lower(), _arg_index)]
             except KeyError:
-                pseudo_table_dict[(_arg_table.lower(), _arg_index)] = {}
+                pseudo_table_dict[(_arg_table_name.lower(), _arg_index)] = {}
 
-            tmp = pseudo_table_dict[(_arg_table.lower(), _arg_index)]
-            tmp[_k.lower()] = (dig_dict(_arg_instance_dict, _k, '_'))
+            tmp = pseudo_table_dict[(_arg_table_name.lower(), _arg_index)]
+            tmp[_column_name.lower()] = (dig_dict(_arg_instance_dict, _column_name, '_'))
             tmp["_id"] = _arg_id
             tmp["_timestamp"] = _arg_timestamp
             tmp["_index"] = _arg_index
             tmp["_join_key"] = _arg_join_key
-            pseudo_table_dict[(_arg_table.lower(), _arg_index)] = tmp
-            # print()
+            pseudo_table_dict[(_arg_table_name.lower(), _arg_index)] = tmp
 
 for reservation in responce['Reservations']:
     for instance in reservation['Instances']:
-
-        flat_dict = {}
-
-        for _k_1 in list(get_dict_hierarchy(instance, '', '_')):
-            if isinstance(dig_dict(instance, _k_1, '_'), list):
-                for i in dig_dict(instance, _k_1, '_'):
-                    for _k_2 in list(get_dict_hierarchy(i, '', '_')):
-                        if isinstance(dig_dict(i, _k_2, '_'), list):
-                            pass
-                        else:
-                            flat_dict[_k_1 + _k_2] = dig_dict(i, _k_2, '_')
-            else:
-                flat_dict[_k_1] = dig_dict(instance, _k_1, '_')
-
-        pprint.pprint(flat_dict)
-
         _id = instance["InstanceId"]
-        _f(instance, "describe_instances", None, _id, _timestamp, None)
-        pprint.pprint(pseudo_table_dict)
+        _index = None
+        _join_key = None
+        _flatten(instance, "describe_instances", _id, _timestamp, _index, _join_key)
 
+        # pprint.pprint(pseudo_table_dict)
+
+        for _k, _v in pseudo_table_dict.items():
+            table_name = _k[0]
+            tmp_column_name_list = list(_v.keys())
+            column_name_list = map(avoid_key_name, tmp_column_name_list)
+            # print(table_name)
+            # print(_v.keys())
+
+            tmp_create_statement_list = list(map(lambda x : x + " VARCHAR(" + VARCHAR_LENGTH + ")", column_name_list))
+            # print(tmp_create_statement_list)
+
+            create_table = """
+                    CREATE TABLE {0}.{1}({2}, PRIMARY KEY(_id, _timestamp, _index, _join_key));""".format(
+                            'root',
+                            table_name,
+                            ','.join(tmp_create_statement_list))
+            # print(create_table)
+
+            db.create(create_table)
+            db.connection.commit()
+
+
+            column_name_list = list()
+            value_list = list()
+
+            id = None
+            print(_v)
+            for k, v in _v.items():
+                if len(k) <= 63:
+                    if k.lower()== 'primary' or k=='DEFAULT' or k=='END':
+                        column_name_list.append('_' + k.lower())
+                    else:
+                        column_name_list.append(k.lower())
+                else:
+                    # Column length is up to 63, this is restriction of PostgreSQL.
+                    # Inserting another if block is annoying, so just set 63 at here.
+                    # So, some column name might be truncated.
+                    column_name_list.append(k[-63:])
+                try:
+                    str(v).encode('ascii')
+                    value_list.append(str(v))
+                except UnicodeEncodeError as e:
+                    show_message(type(e))
+                    show_message(e.args)
+                    value_list.append('?')
+
+            tmp_insert_table = """
+                    INSERT INTO {0}.{1} ({2}) VALUES(%s""".format(
+                            'root',
+                            table_name,
+                            ','.join(list(map(lambda x : x.lower(), column_name_list))))
+            count = 1
+            for _x in range(1, len(column_name_list)):
+                tmp_insert_table += ', %s'
+                count += 1
+            insert_table = tmp_insert_table + ');'
+            print(insert_table)
+
+            db.insert(insert_table, value_list)
+            db.connection.commit()
---
+++ requester/util.py
@@ -0,0 +1,129 @@
+import datetime
+import io
+import json
+import os
+import psycopg2
+import re
+import sys
+import time
+import traceback
+
+from fluent import sender
+from fluent import event
+
+DB_CONNECTION_RETRY = 3
+DB_HOST = "postgres"
+DB_NAME = os.environ['POSTGRES_DB_NAME']
+DB_PORT = 5432
+DB_ROOT_PASSWORD = os.environ['POSTGRES_SUPERUSER_PASSWORD']
+DB_ROOT_SCHEMA = os.environ['POSTGRES_SUPERUSER_SCHEMA']
+DB_ROOT_USER = os.environ['POSTGRES_SUPERUSER']
+HOSTNAME = os.environ['HOSTNAME']
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+def show_message(msg, newline=True, show_timestamp=True, _arg_function='', _arg_function_enter=0, _arg_function_exit=0, _arg_callback_id=''):
+    try:
+        sys.stdout.write(
+            "%s%s%s%s" % (
+                ("%s : " % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) if show_timestamp else "",
+                "[%s]" % HOSTNAME[:4],
+                msg,
+                "\n" if newline else ""))
+        sys.stdout.flush()
+
+        body_dict = dict()
+        body_dict['callback_id'] = _arg_callback_id
+        body_dict['function'] = _arg_function
+        body_dict['function_enter'] = _arg_function_enter
+        body_dict['function_exit'] = _arg_function_exit
+        body_dict['hostname'] = HOSTNAME
+        body_dict['message'] = str(msg)
+        now = datetime.datetime.now()
+        body_dict['@timestamp'] = now.strftime("%Y-%m-%dT%H:%M:%S.") + "%03d" % (now.microsecond // 1000)
+
+        sender.setup(
+                'awssql.logs',
+                host = 'localhost',
+                port=8888)
+        event.Event(
+                'follow',
+                body_dict)
+    except UnicodeEncodeError:
+        show_message(type(e))
+        show_message(e.args)
+        traceback.print_exc()
+
+
+class DB:
+    def __init__(self, _arg_user=DB_ROOT_USER, _arg_password=DB_ROOT_PASSWORD):
+        retry_count = 1
+        while retry_count <= int(DB_CONNECTION_RETRY):
+            try:
+                self.connection = psycopg2.connect(
+                        "host={0} port={1} user={2} password={3} dbname={4}"
+                        .format(DB_HOST, DB_PORT, _arg_user, _arg_password, DB_NAME))
+                break
+            except psycopg2.OperationalError as e:
+                exception_message_list = list()
+                for _x in e.args:
+                    exception_message_list.append(str(_x))
+                exception_messages = ','.join(exception_message_list)
+                postgres_cannot_connect_server = re.search(r"could not connect to server", exception_messages)
+                if postgres_cannot_connect_server:
+                    show_message(type(e))
+                    show_message(e.args)
+                    show_message("Retry count is {0}, going to wait {1} second(s).".format(retry_count, retry_count**2))
+                    time.sleep(retry_count**2)
+                    retry_count += 1
+                else:
+                    traceback.print_exc()
+                    exit()
+
+        self.cursor = self.connection.cursor()
+
+
+    def __del__(self):
+        self.cursor.close()
+        self.connection.close()
+
+
+    def create(self, _arg_sql_create_statement):
+        try:
+            self.cursor.execute(_arg_sql_create_statement)
+            self.connection.commit()
+        except psycopg2.ProgrammingError as e:
+            exception_message_list = list()
+            for _x in e.args:
+                exception_message_list.append(str(_x))
+            exception_messages = ','.join(exception_message_list)
+            postgres_table_already_exists = re.search(r"relation .* already", exception_messages)
+            postgres_schema_already_exists = re.search(r"schema .* already exists", exception_messages)
+            postgres_role_already_exists = re.search(r"role .* already exists", exception_messages)
+            cannot_drop_columns_from_view = re.search(r"cannot drop columns from view", exception_messages)
+            if postgres_table_already_exists or postgres_schema_already_exists or postgres_role_already_exists or cannot_drop_columns_from_view:
+                show_message(type(e))
+                show_message(e.args)
+            else:
+                traceback.print_exc()
+                exit()
+
+
+    # Expect that value passed as list.
+    def insert(self, _arg_sql_insert_statement, _arg_insert_value_list):
+        try:
+            self.cursor.execute(
+                    _arg_sql_insert_statement,
+                    _arg_insert_value_list)
+        except psycopg2.IntegrityError as e:
+            exception_message_list = list()
+            for _x in e.args:
+                exception_message_list.append(str(_x))
+            exception_messages = ','.join(exception_message_list)
+            postgres_duplicate_entry = re.search(r"duplicate key value violates unique constraint .*", exception_messages)
+            if postgres_duplicate_entry:
+                show_message(type(e))
+                show_message(e.args)
+            else:
+                traceback.print_exc()
+                exit()
