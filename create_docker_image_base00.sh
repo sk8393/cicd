6827faccb325bfce48a1d2fa8fe8aa47c59bbfbf
Ubuntu <ubuntu@ip-172-31-10-6.ap-northeast-1.compute.internal>
2021-04-28 12:08:36 +0000

    Modified base00/Dockerfile and other shell scripts to stop Jenkins job when docker build fails.

--- create_docker_image_base00.sh
+++ create_docker_image_base00.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 docker build -t sk8393/base00:test -f base00/Dockerfile .
+if [ $? -ne 0 ]; then
+    exit 1
+fi
--- create_docker_image_base02.sh
+++ create_docker_image_base02.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 source ./create_docker_image_base01.sh
 docker build -t sk8393/base02:test -f base02/Dockerfile .
+if [ $? -ne 0 ]; then
+    exit 1
+fi
